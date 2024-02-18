c6c008865cd53c55355e6f7d994a7d9e0a1da801
fabiomtm <160417175+fabiomtm@users.noreply.github.com>
2024-02-18 16:46:09 +0000

    Add files via upload

---
+++ sloeber.ino.cpp
@@ -0,0 +1,32 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2024-01-11 15:30:38
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_task_wdt.h"
+#include "funcoes_barra_leds.h"
+#include <FastLED.h>
+#include <ESP32_Servo.h>
+
+void setup() ;
+void loop() ;
+void vDisplay(void *pvParameters) ;
+void vVelocidade(void *pvParamenters) ;
+void vDRS(void *pvParameters) ;
+void vRPM(void *pvParameters) ;
+void vBotao(void *pvParameters) ;
+void vLED(void *pvParameters) ;
+void Botao_int() ;
+
+#include "Projeto_seev_Bruno_Fabio.ino"
+
+
+#endif
